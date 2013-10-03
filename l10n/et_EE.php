63c6d23a4189012d842c401c5e70a3af0d7e63c7
Oliver Gasser <oliver.gasser@gmail.com>
2013-10-03 17:35:40 +0200

    Cloned current version of mozilla_sync app
    Cloned the mozilla_sync app from owncloud/apps using the last commit [1] as the base.
    
    [1] https://github.com/owncloud/apps/commit/e6fcd9eac174ca7531543f844f52232ef8dfebbd

---
+++ l10n/et_EE.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"Mozilla Sync Service" => "Mozilla sünkroniseerimise teenus",
+"Please set your email address in account settings" => "Palun sisesta konto seadetesse oma e-posti aadress",
+"Email" => "E-post",
+"Password and Confirmation" => "Parool ja kinnitus",
+"Server address" => "Serveri aadress"
+);
