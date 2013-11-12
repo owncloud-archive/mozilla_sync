34ffc963eada1e8d85e349b2c47651762fe3026d
Oliver Gasser <oliver.gasser@gmail.com>
2013-11-12 12:18:45 +0100

    make string translatable

--- templates/settings.php
+++ templates/settings.php
@@ -1,14 +1,13 @@
 <fieldset class="personalblock">
 	<legend><?php p($l->t('Mozilla Sync')); ?></legend>
     <table class="nostyle">
-
       <tr>
         <td><label class="bold"><?php p($l->t('Email'));?></label></td>
         <td><?php p($_['email']);?></td>
       </tr>
       <tr>
         <td><label class="bold"><?php p($l->t('Password'));?></label></td>
-        <td>Use your owncloud account password</td>
+        <td><?php p($l->t('Use your ownCloud account password'));?></td>
       </tr>
       <tr>
         <td><label class="bold"><?php p($l->t('Server address'));?></label></td>
