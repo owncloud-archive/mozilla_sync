b30b0512fb9a5eadbbd44ca944e42fb8880f02f7
Mr. Jenkins <jenkins@owncloud.org>
2013-11-21 21:13:50 -0500

    [tx-robot] updated from transifex

--- l10n/pt_BR.php
+++ l10n/pt_BR.php
@@ -1,5 +1,9 @@
 <?php $TRANSLATIONS = array(
+"Mozilla Sync" => "Sincronização Mozilla",
+"Please set an email address in your account settings" => "Por favor, defina um endereço de e-mail nas configurações da conta",
 "Email" => "E-mail",
 "Password" => "Senha",
-"Server address" => "Endereço do servidor"
+"Use your ownCloud account password" => "Use a senha da conta ownCloud",
+"Server address" => "Endereço do servidor",
+"Once set up, additional devices can be added via Mozilla's device pairing service or manually." => "Uma vez configurado, dispositivos adicionais podem ser adicionados através do serviço de emparelhamento do dispositivo do Mozilla ou manualmente."
 );
--- l10n/ru.php
+++ l10n/ru.php
@@ -1,5 +1,9 @@
 <?php $TRANSLATIONS = array(
+"Mozilla Sync" => "Mozilla Sync",
+"Please set an email address in your account settings" => "Пожалуйста укажите адрес электронной почты в настройках учётной записи",
 "Email" => "E-mail",
 "Password" => "Пароль",
-"Server address" => "Адрес сервера"
+"Use your ownCloud account password" => "Используйте пароль учётной записи OwnCloud",
+"Server address" => "Адрес сервера",
+"Once set up, additional devices can be added via Mozilla's device pairing service or manually." => "После настройки можно будет добавить другие устройства через услугу привязки от Mozilla либо вручную"
 );
