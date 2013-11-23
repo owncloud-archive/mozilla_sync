a96c004300264fe3f2e39fc7e0d90074e31748cd
Oliver Gasser <oliver@flowriver.net>
2013-11-23 17:45:51 +0100

    Cleanup in remote.php

--- appinfo/remote.php
+++ appinfo/remote.php
@@ -4,36 +4,33 @@
  * This is called by Sync clients when they access the Sync API
  * on this ownCloud server.
  *
- * @author Michal Jaskurzynski 
+ * @author Michal Jaskurzynski
  * @author Oliver Gasser
  */
 
-// Load user backends
-OC_App::loadApps($RUNTIME_APPTYPES);
-
-// Get sync URL
+// Get Sync URL
 $url = OCA\mozilla_sync\Utils::getSyncUrl();
-if($url === false) {
+if ($url === false) {
 	OCA\mozilla_sync\Utils::changeHttpStatus(404);
 	exit();
 }
 
 // Parse and validate the URL accessed by the client
 $urlParser = new OCA\mozilla_sync\UrlParser($url);
-if(!$urlParser->isValid()) {
+if (!$urlParser->isValid()) {
 	OCA\mozilla_sync\Utils::changeHttpStatus(404);
 	exit();
 }
 
-// Get service type based on URL and determine whether to start user or storage service 
+// Get service type based on URL and determine whether to start user or storage service
 $service = OCA\mozilla_sync\Utils::getServiceType();
 
-if($service === 'userapi') {
+if ($service === 'userapi') {
 	// Send a timestamp header
 	OCA\mozilla_sync\Utils::sendMozillaTimestampHeader();
 	$userService = new OCA\mozilla_sync\UserService($urlParser);
 	$userService->run();
-} else if($service === 'storageapi') {
+} else if ($service === 'storageapi') {
 	// Note: Timestamp header will be sent later by storage API service
 	$storageService = new OCA\mozilla_sync\StorageService($urlParser);
 	$storageService->run();
