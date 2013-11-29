559ba6ef1f87bc3d87af8b2ba0dc7cefeafb6820
Mr. Jenkins <jenkins@owncloud.org>
2013-11-29 14:16:07 -0500

    [tx-robot] updated from transifex

--- l10n/uk.php
+++ l10n/uk.php
@@ -1,7 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Mozilla Sync" => "Синхронізація Mozilla",
+"Please set an email address in your account settings" => "Будь ласка, вкажіть адресу електронної пошти в налаштуваннях облікового запису",
 "Email" => "Ел.пошта",
 "Password" => "Пароль",
 "Use your ownCloud account password" => "Використовуйте свій пароль облікового запису ownCloud",
-"Server address" => "Адреса сервера"
+"Server address" => "Адреса сервера",
+"Once set up, additional devices can be added via Mozilla's device pairing service or manually." => "Після першого налаштування, додаткові пристрої можуть бути додані за допомогою служби спарювання Mozilla, або вручну."
 );
