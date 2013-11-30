198195746369ecda4b3512d236371ae40e8549c5
RoboMod <mod.andy@gmx.de>
2013-11-30 18:49:16 +0100

    fixed spacing, lines, naming

--- ajax/deletestorage.php
+++ ajax/deletestorage.php
@@ -8,6 +8,9 @@
 *
 */
 
+// Check if user is logged in
+\OCP\JSON::checkLoggedIn();
+
 // Check if valid requesttoken was sent
 \OCP\JSON::callCheck();
 
@@ -15,27 +18,27 @@
 $l=OC_L10N::get('core');
 
 // Get userId and try to delete the user
-$userId = \OCA\mozilla_sync\User::userNameToUserId(\OCP\User::getUser());
-if ($userId) {
-    // delete storage and user
-    if (\OCA\mozilla_sync\Storage::deleteStorage($userId) === false) {
-        // Send error message
-        \OCP\JSON::error(array( "data" => array( "message" => $l->t("Failed to delete storage") )));
-    }
-    else {
-        if (\OCA\mozilla_sync\User::deleteUser($userId) === false) {
-            // Send error message
-            \OCP\JSON::error(array( "data" => array( "message" => $l->t("Failed to delete user") )));
-        }
-        else {
-            // Send success message
-            \OCP\JSON::success(array( "data" => array( "message" => $l->t("Storage deleted") )));
-        }
-    }
+$syncId = \OCA\mozilla_sync\User::userNameToSyncId(\OCP\User::getUser());
+if ($syncId) {
+	// delete storage and user
+	if (\OCA\mozilla_sync\Storage::deleteStorage($syncId) === false) {
+		// Send error message
+		\OCP\JSON::error(array( "data" => array( "message" => $l->t("Failed to delete storage") )));
+	}
+	else {
+		if (\OCA\mozilla_sync\User::deleteUser($syncId) === false) {
+			// Send error message
+			\OCP\JSON::error(array( "data" => array( "message" => $l->t("Failed to delete user") )));
+		}
+		else {
+			// Send success message
+			\OCP\JSON::success(array( "data" => array( "message" => $l->t("Storage deleted") )));
+		}
+	}
 }
 else {
-    // Send error message
-    \OCP\JSON::error(array( "data" => array( "message" => $l->t("User not found") )));
+	// Send error message
+	\OCP\JSON::error(array( "data" => array( "message" => $l->t("User not found") )));
 }
 
 /* vim: set ts=4 sw=4 tw=80 noet : */
