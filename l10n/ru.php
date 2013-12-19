0efc851660e95c0ad523f250ec6ca4754bc430d1
Mr. Jenkins <jenkins@owncloud.org>
2013-12-19 02:06:49 -0500

    [tx-robot] updated from transifex

--- l10n/ru.php
+++ l10n/ru.php
@@ -1,8 +1,16 @@
 <?php $TRANSLATIONS = array(
+"Failed to delete storage" => "Ошибка при удалении хранилища",
+"Failed to delete user" => "Ошибка при удалении пользователя",
+"Storage deleted" => "Хранилище удалено",
+"User not found" => "Пользователь не найден",
+"Invalid input" => "Не верный ввод",
+"Quota saved" => "Квота сохранена",
 "Mozilla Sync" => "Mozilla Sync",
+"When activated, only members of this group can access Mozilla Sync." => "Когда активировано, только члены группы имеют доступ к Mozilla Sync.",
 "0 is unlimited" => "0 - без ограничений",
 "Email" => "E-mail",
 "Password" => "Пароль",
 "Use your ownCloud account password" => "Используйте пароль учётной записи OwnCloud",
-"Server address" => "Адрес сервера"
+"Server address" => "Адрес сервера",
+"Delete storage" => "Удалить хранилище"
 );
