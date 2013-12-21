77eb53df79a6e15b2d5de243003bafc6a41caaf8
Mr. Jenkins <jenkins@owncloud.org>
2013-12-21 02:06:54 -0500

    [tx-robot] updated from transifex

--- l10n/ko.php
+++ l10n/ko.php
@@ -1,7 +1,16 @@
 <?php $TRANSLATIONS = array(
+"Failed to delete storage" => "저장소 삭제에 실패함",
+"Failed to delete user" => "사용자 삭제에 실패함",
+"Storage deleted" => "저장소 삭제됨",
+"User not found" => "사용자를 찾을수 없음",
+"Invalid input" => "잘못된 입력",
 "Mozilla Sync" => "Mozilla Sync",
+"When activated, only members of this group can access Mozilla Sync." => "이를 허용할시, 이 그룹의 구성원들만 Mozilla Sync에 접근 가능합니다.",
 "0 is unlimited" => "0은 무제한입니다",
+"Installed version" => "설치된 버전",
 "Email" => "이메일",
 "Password" => "암호",
-"Server address" => "서버 주소"
+"Server address" => "서버 주소",
+"Last sync" => "마지막 동기화",
+"Delete storage" => "저장소 삭제"
 );
--- l10n/pl.php
+++ l10n/pl.php
@@ -1,8 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Mozilla Sync" => "Mozilla Sync",
 "0 is unlimited" => "0 - bez limitów",
+"Installed version" => "Zainstalowana wersja",
 "Email" => "Email",
 "Password" => "Hasło",
 "Use your ownCloud account password" => "Użyj twoje hasło konta ownCloud",
-"Server address" => "Adres Serwera"
+"Server address" => "Adres Serwera",
+"Last sync" => "Ostatnia synchronizacja"
 );
