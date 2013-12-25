888c5760f9439f2dfdb7e5967c70d3f7acc60f70
Mr. Jenkins <jenkins@owncloud.org>
2013-12-25 02:06:25 -0500

    [tx-robot] updated from transifex

--- l10n/da.php
+++ l10n/da.php
@@ -3,7 +3,9 @@
 "Invalid input" => "Ugyldigt indput",
 "Mozilla Sync" => "Mozilla Sync",
 "0 is unlimited" => "0 er ubegrænset",
+"Installed version" => "Installeret version",
 "Email" => "E-mail",
 "Password" => "Kodeord",
+"Use your ownCloud account password" => "Brug kodeordet til din ownCloud konto",
 "Server address" => "Serveradresse"
 );
