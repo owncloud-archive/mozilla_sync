c55ebab0f664d98ca0c484bdd9b3444f0db507e1
Mr. Jenkins <jenkins@owncloud.org>
2014-02-13 03:19:33 -0500

    [tx-robot] updated from transifex

--- l10n/cs_CZ.php
+++ l10n/cs_CZ.php
@@ -11,6 +11,7 @@
 "Restrict to group" => "Omezit na skupinu",
 "When activated, only members of this group can access Mozilla Sync." => "Po aktivaci mohou pouze členové této skupiny přistupovat k Mozilla Sync.",
 "Sync quota" => "Kvóta synchronizace",
+"Set the value to 0 for unlimited quota." => "Zadejte hodnotu 0 pro neomezenou kvótu.",
 "Error! You are not allowed to use Mozilla Sync! You need to be a member of the %s group." => "Chyba! Nemáte povoleno použít Mozilla Sync! Musíte být členem skupiny %s.",
 "To set up Mozilla Sync create a new Sync account in Firefox." => "Pro nastavení Mozilla Sync vytvořte nový Sync účet ve Firefoxu.",
 "Mozilla Sync is set up, additional devices can be added via Mozilla's device pairing service or manually." => "Mozilla Sync je nastaven. Další zařízení mohou být přidána přes službu párování zařízení (Mozilla) nebo ručně.",
@@ -25,6 +26,6 @@
 "Size of stored data" => "Velikost uložených dat",
 "No data stored yet." => "Zatím nebyla uložena žádná data.",
 "Number of synced devices" => "Počet synchronizovaných zařízení",
-"Delete Sync data" => "Smazat Sync data",
+"Delete Sync data" => "Smazat data synchronizace",
 "Attention! This will delete all your Sync data on the server." => "Pozor! Toto smaže všechna vaše Sync data na serveru."
 );
