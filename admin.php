30773c93e39ec2add862cfcb7251c6ae2bd885f7
Oliver Gasser <oliver@flowriver.net>
2014-02-14 11:53:41 +0100

    Remove unneeded variable from template

--- admin.php
+++ admin.php
@@ -12,7 +12,6 @@ $tmpl = new \OCP\Template('mozilla_sync', 'admin');
 
 $tmpl->assign('mozillaSyncRestrictGroupEnabled', \OCA\mozilla_sync\User::getAuthorizedGroup());
 $tmpl->assign('mozillaSyncQuota', \OCA\mozilla_sync\User::getQuota());
-$tmpl->assign('mozillaSyncVersion', \OCP\App::getAppVersion('mozilla_sync'));
 
 return $tmpl->fetchPage();
 
