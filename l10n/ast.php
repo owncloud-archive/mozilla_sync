7e1d7cf97805f4ebc0062b81b1e7dc6786e38883
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-04-26 02:05:58 -0400

    [tx-robot] updated from transifex

--- l10n/ast.php
+++ l10n/ast.php
@@ -1,4 +1,12 @@
 <?php $TRANSLATIONS = array(
+"Failed to delete user" => "Fallo al desaniciar al usuariu",
+"User not found" => "Usuariu non alcontráu",
+"Invalid input" => "Entrada inválida",
+"When activated, only members of this group can access Mozilla Sync." => "Cuando s'active, namái los miembros d'esti grupu puen acceder a Mozilla Sync.",
+"Set the value to 0 for unlimited quota." => "Afitar el valor 0 pa cuota illimitada.",
 "Email" => "Corréu-e",
-"Password" => "Contraseña"
+"Password" => "Contraseña",
+"No data stored yet." => "Entá nun hai datos atroxaos",
+"Number of synced devices" => "Númberu de preseos sincronizaos",
+"Attention! This will delete all your Sync data on the server." => "¡Atención! Esto desaniciará tolos tos datos de Sync nel sirvidor."
 );
