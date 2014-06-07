bbbef3f42b1da0211dccea3c141a7ad5d8fe6770
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-07 02:05:31 -0400

    [tx-robot] updated from transifex

--- l10n/cs_CZ.php
+++ l10n/cs_CZ.php
@@ -15,12 +15,17 @@
 "Error! You are not allowed to use Mozilla Sync! You need to be a member of the %s group." => "Chyba! Nemáte povoleno použít Mozilla Sync! Musíte být členem skupiny %s.",
 "To set up Mozilla Sync create a new Sync account in Firefox." => "Pro nastavení Mozilla Sync vytvořte nový Sync účet ve Firefoxu.",
 "Mozilla Sync is set up, additional devices can be added via Mozilla's device pairing service or manually." => "Mozilla Sync je nastaven. Další zařízení mohou být přidána přes službu párování zařízení (Mozilla) nebo ručně.",
+"Email:" => "E-mail:",
 "Has to be unique among all Sync users" => "Musí být jedinečný mezi všemi uživateli",
 "Error! Duplicate email addresses detected! Email addresses need to be unique for Mozilla Sync to work." => "Chyba! Byla detekována duplicitní e-mailová adresa! E-mailová adresa musí být unikátní pro použití Mozilla Sync.",
 "Password:" => "Heslo:",
 "Use your ownCloud account password" => "Použijte heslo svého ownCloud účtu",
+"Server address:" => "Adresa serveru:",
 "Sync Status" => "Stav synchronizace",
+"Last sync:" => "Poslední synchronizace:",
+"Size of stored data:" => "Velikost uložených dat:",
 "No data stored yet." => "Zatím nebyla uložena žádná data.",
+"Number of synced devices:" => "Počet synchronizovaných zařízení:",
 "Delete Sync data" => "Smazat data synchronizace",
 "Attention! This will delete all your Sync data on the server." => "Pozor! Toto smaže všechna vaše Sync data na serveru."
 );
