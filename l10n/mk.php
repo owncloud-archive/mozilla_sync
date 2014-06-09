97fbebeda524fdc443f9d975db2cb7fca99e6508
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-09 02:05:30 -0400

    [tx-robot] updated from transifex

--- l10n/mk.php
+++ l10n/mk.php
@@ -1,3 +1,18 @@
 <?php $TRANSLATIONS = array(
-"Password:" => "Лозинка:"
+"Failed to delete storage" => "Неуспешно бришење на податоците",
+"Failed to delete user" => "Неуспешно бришење на корисник",
+"Storage deleted" => "Податоците се избришани",
+"User not found" => "Корисникот не е најден",
+"Invalid input" => "Неправилен внес",
+"Quota saved" => "Квотата е снимена",
+"Mozilla Sync" => "Мозила синхронизација",
+"Restrict to group" => "Ограничи на група",
+"Email:" => "Електронска пошта:",
+"Password:" => "Лозинка:",
+"Use your ownCloud account password" => "Користи го твојота лозинка за ownCloud",
+"Server address:" => "Адреса на серверот:",
+"Sync Status" => "Статус на синхронизација",
+"Last sync:" => "Последен пат синхронизирано:",
+"Size of stored data:" => "Големина на снимените податоци:",
+"Number of synced devices:" => "Број на синхронизирани уреди:"
 );
