c216654bdc43a478286bacfeb2a8f39eba00e3bf
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-18 02:05:30 -0400

    [tx-robot] updated from transifex

--- l10n/ca.php
+++ l10n/ca.php
@@ -15,12 +15,17 @@
 "Error! You are not allowed to use Mozilla Sync! You need to be a member of the %s group." => "Error! No esteu autoritzat per utilitzar Mozilla Sync! Cal que sigueu membre del grup %s.",
 "To set up Mozilla Sync create a new Sync account in Firefox." => "Per configurar Mozilla Sync, creeu un compte de sincronització al Firefox.",
 "Mozilla Sync is set up, additional devices can be added via Mozilla's device pairing service or manually." => "Mozilla Sync està configurat. Es poden afegir dispositius addicionals manualment o a través del servei d'emparellament de Mozilla.",
+"Email:" => "Correu electrònic:",
 "Has to be unique among all Sync users" => "Ha de ser únic entre tots els usuaris del Sync",
 "Error! Duplicate email addresses detected! Email addresses need to be unique for Mozilla Sync to work." => "Error! S'han detectat adreces de correu duplicades! Les adreces de correu electrònic han de ser úniques perquè funcioni Mozilla Sync.",
 "Password:" => "Contrasenya:",
 "Use your ownCloud account password" => "Usa la contrasenya del compte ownCloud",
+"Server address:" => "Adreça del servidor:",
 "Sync Status" => "Estat de la sincronització",
+"Last sync:" => "Última sincronització:",
+"Size of stored data:" => "Mida de les dades emmagatzemades:",
 "No data stored yet." => "Encara no s'han desat dades",
+"Number of synced devices:" => "Nombre de dispositius sincronitzats:",
 "Delete Sync data" => "Suprimeix les dades del Sync",
 "Attention! This will delete all your Sync data on the server." => "Atenció! això suprimirà totes les dades del Sync al servidor."
 );
