fd96b151f2d82c7304d56bfedbe0d8cc9d9433f5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 02:05:30 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"Failed to delete storage" => "Informasiya daşıyıcısını silmək mümkün olmadı",
+"Failed to delete user" => "İstifadəçini silmək mümkün olmadı",
+"Storage deleted" => "İnformasiya daşıyıcısı silindi",
+"User not found" => "İstifadəçi tapılmadı",
+"Invalid input" => "Səhv daxil edildi"
+);
