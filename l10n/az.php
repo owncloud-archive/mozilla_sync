aa9160736fcbf77690d09eab84479e0c107a590e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-19 02:05:30 -0400

    [tx-robot] updated from transifex

--- l10n/az.php
+++ l10n/az.php
@@ -3,5 +3,8 @@
 "Failed to delete user" => "İstifadəçini silmək mümkün olmadı",
 "Storage deleted" => "İnformasiya daşıyıcısı silindi",
 "User not found" => "İstifadəçi tapılmadı",
-"Invalid input" => "Səhv daxil edildi"
+"Invalid input" => "Səhv daxil edildi",
+"Mozilla Sync" => "Mozilla Sync",
+"Restrict to group" => "Qrup üçün yetkini məhdudlaşdır",
+"Sync quota" => "Sinxronizasiya məhdudiyyəti"
 );
