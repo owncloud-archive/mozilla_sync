ee86b0fd60f1b7354ebbb722a30ce4189c52bb59
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-30 06:57:36 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "mozilla_sync",
+    {
+    "Failed to delete storage" : "저장소를 삭제할 수 없음",
+    "Failed to delete user" : "사용자를 삭제할 수 없음",
+    "Storage deleted" : "저장소 삭제됨",
+    "User not found" : "사용자를 찾을 수 없음",
+    "Restriction saved" : "접근 제한 저장됨",
+    "Invalid input" : "잘못된 입력",
+    "Sync email saved" : "싱크 email 저장됨",
+    "Quota saved" : "할당량 저장됨",
+    "Mozilla Sync" : "Mozilla Sync",
+    "Restrict to group" : "그룹으로 제한",
+    "When activated, only members of this group can access Mozilla Sync." : "사용하면 이 그룹 구성원만 Mozilla Sync에 접근할 수 있습니다.",
+    "Sync quota" : "동기화 할당량",
+    "Error! You are not allowed to use Mozilla Sync! You need to be a member of the %s group." : "오류! Mozilla Sync를 사용할 수 없습니다! %s 그룹의 구성원이어야 합니다.",
+    "To set up Mozilla Sync create a new Sync account in Firefox." : "Mozilla Sync를 사용하려면 Firefox에서 새로운 Sync 계정을 설정하십시오.",
+    "Mozilla Sync is set up, additional devices can be added via Mozilla's device pairing service or manually." : "Mozilla Sync를 설정하였고, 장치를 추가하려면 Mozilla의 장치 페어링 서비스를 사용하거나 수동으로 추가하십시오.",
+    "Error! Duplicate email addresses detected! Email addresses need to be unique for Mozilla Sync to work." : "오류! 중복된 이메일 주소입니다! Mozilla Sync를 사용하려면 유일한 이메일 주소를 사용해야 합니다.",
+    "Password:" : "암호:",
+    "Use your ownCloud account password" : "ownCloud 계정 암호 사용",
+    "Sync Status" : "싱크 상태",
+    "No data stored yet." : "데이터가 저장되지 않았습니다.",
+    "Delete Sync data" : "싱크 데이터 삭제",
+    "Attention! This will delete all your Sync data on the server." : "알림! 서버에 저장된 모든 동기화 데이터를 삭제합니다."
+},
+"nplurals=1; plural=0;");
