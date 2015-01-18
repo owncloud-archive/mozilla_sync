fd9784875ea826c4d2186c50b826a2c4d12b8722
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-18 02:05:13 -0500

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -24,15 +24,15 @@ $TRANSLATIONS = array(
 "Email:" => "Adresse courriel :",
 "Has to be unique among all Sync users" => "Doit être unique parmi tous les utilisateurs de Sync",
 "Error! Duplicate email addresses detected! Email addresses need to be unique for Mozilla Sync to work." => "Erreur ! Un doublon d'adresse e-mail a été détecté ! Les adresses e-mail doivent être uniques pour que Mozilla Sync puisse fonctionner.",
-"Password:" => "Mot de passe FTP :",
-"Use your ownCloud account password" => "Utiliser votre mot de passe ownCloud",
+"Password:" => "Mot de passe :",
+"Use your ownCloud account password" => "Utilisez votre mot de passe ownCloud",
 "Server address:" => "Adresse du serveur :",
 "Sync Status" => "Statut de synchronisation",
 "Last sync:" => "Dernière synchronisation :",
 "Size of stored data:" => "Taille des données stockées :",
 "No data stored yet." => "Aucune donnée stockée.",
 "Number of synced devices:" => "Nombre d'appareils synchronisés :",
-"Delete Sync data" => "Supprimer les données de synchronisation",
+"Delete Sync data" => "Supprimer les données",
 "Attention! This will delete all your Sync data on the server." => "Attention ! Cela va supprimer toutes vos données Sync sur le serveur."
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
