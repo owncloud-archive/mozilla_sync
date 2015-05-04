db1e25cb2f6fa86cf3f27e6ba0340c944634ffa6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-05-04 02:05:18 -0400

    [tx-robot] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -36,4 +36,4 @@ OC.L10N.register(
     "Delete Sync data" : "Удалить данные Sync",
     "Attention! This will delete all your Sync data on the server." : "Внимание! Это действие удалит все ваши данные Sync на сервере."
 },
-"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
+"nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
