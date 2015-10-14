d32cac9b817dc12c15acbd7df644b3a5c1fc1c3f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-14 02:05:22 -0400

    [tx-robot] updated from transifex

--- l10n/sq.php
+++ l10n/sq.php
@@ -1,4 +1,38 @@
-<?php $TRANSLATIONS = array(
-"Email" => "Email-i",
-"Password" => "fjalëkalim"
+<?php
+$TRANSLATIONS = array(
+"Failed to delete storage" => "Fshirja e depos dështoi",
+"Failed to delete user" => "Fshirja e përdoruesit dështoi",
+"Storage deleted" => "Depozita u fshi",
+"User not found" => "S’u gjet përdorues",
+"Restriction saved" => "Kufizimet u ruajtën",
+"Invalid input" => "Futje e pavlefshme",
+"Auto create sync account enabled" => "Vetëkrijimi i llogarive Sync u aktivizua",
+"Auto create sync account disabled" => "Vetëkrijimi i llogarive Sync u çaktivizua",
+"Sync email saved" => "Email-i Sync u ruajt",
+"Quota saved" => "Kuotat u ruajtën",
+"Mozilla Sync" => "Mozilla Sync",
+"Restrict to group" => "Kufizoje për grupin",
+"When activated, only members of this group can access Mozilla Sync." => "Po u aktivizua, vetëm anëtarë të këtij grupi mund të hyjnë te Mozilla Sync-u.",
+"Sync quota" => "Kuota Sync-u",
+"Set the value to 0 for unlimited quota." => "Për kuota të pakufizuara jepini vlerën 0.",
+"Auto create sync account" => "Vetëkrijo llogari Sync",
+"When activated, the Mozilla Sync registration API will be disabled and instead accounts will be auto created using a user's existing credentials" => "Kur të aktivizohet, API për regjistrim te Mozilla Sync-u do të çaktivizohet dhe llogaritë do të vetëkrijohen duke përdorur kredencialet ekzistuese të përdoruesve",
+"Error! You are not allowed to use Mozilla Sync! You need to be a member of the %s group." => "Gabim! S’ju lejohet të përdorni Mozilla Sync-in! Lypset të jeni anëtar i grupit %s.",
+"To use Mozilla Sync enter the credentials in to Mozilla Sync client" => "Që të përdorni Mozilla Sync-un jepni kredencialet te klienti Mozilla Sync",
+"To set up Mozilla Sync create a new Sync account in Firefox." => "Që të rregulloni Mozilla Sync-n, krijoni te Firefox-i një llogari të re Sync-u.",
+"Mozilla Sync is set up, additional devices can be added via Mozilla's device pairing service or manually." => "Mozilla Sync-u u rregullua, pajisjet shtesë mund të shtohet përmes shërbimit Mozilla të çiftimit të pajisjeve ose dorazi.",
+"Email:" => "Email:",
+"Has to be unique among all Sync users" => "Duhet të jetë unik mes krejt përdoruesve të Sync-ut",
+"Error! Duplicate email addresses detected! Email addresses need to be unique for Mozilla Sync to work." => "Gabim! U kapën adresa email të përsëdytura! Që Mozilla Sync të funksionojë, adresat email lypset të jenë unike.",
+"Password:" => "Fjalëkalim:",
+"Use your ownCloud account password" => "Përdorni fjalëkalimin e llogarisë tuaj ownCloud",
+"Server address:" => "Adresë shërbyesi:",
+"Sync Status" => "Gjendje Njëkohësimi",
+"Last sync:" => "Njëkohësimi i fundit:",
+"Size of stored data:" => "Madhësi të dhënash të depozituara:",
+"No data stored yet." => "Ende pa të dhëna të depozituara.",
+"Number of synced devices:" => "Numër pajisjesh të njëkohësuara:",
+"Delete Sync data" => "Fshi Të dhëna Sync-u",
+"Attention! This will delete all your Sync data on the server." => "Kujdes! Kështu do të fshihen te shërbyesi krejt të dhënat tuaja Sync."
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
